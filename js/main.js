c87d9ef2f6ae2782a99f237e53404f79249e30df
Franklin Antony <franklinanton.007@gmail.com>
2018-06-14 16:22:57 +0530

    instead used functions

--- js/main.js
+++ js/main.js
@@ -61,8 +61,22 @@
         return false;
       }
     }
+    
+    
   }
-
+  
+  function phonenumber(input) {
+    var phoneno = /^\d{10}$/;
+    if ((input.value.match(phoneno))
+        {
+      return true;
+    }
+    else {
+      alert("message");
+      return false;
+    }
+  }
+  
   function showValidate(input) {
     var thisAlert = $(input).parent();
 
